e5c17cf362e26a1cd6a2ada0866147455fba30d9
tiffany jiang <71298683+Tiffany19970413@users.noreply.github.com>
2021-10-18 04:29:02 +0800

    Update ch02_02.sql

--- SELECT_SQLExample/ch02_02.sql
+++ SELECT_SQLExample/ch02_02.sql
@@ -1,18 +1,19 @@
+#指定資料庫
 USE world;
 
-
+#顯示字串與計算結果
 SELECT 'My name is Simon Johnson', 35 * 12;
 
-
+#指定world資料庫的city表格
 SELECT * FROM city;
 
-
+#不使用「USE cmdev」則直接切換cmdev資料庫並指定emp表格
 SELECT * FROM cmdev.emp;
 
-
+# *字號：依表格欄位排序回傳查詢資料（設計表格時決定排序）
 SELECT * FROM city;
 
-
+#依照需求決定city表格的查詢欄位排序
 SELECT ID, Name
 FROM   city;
 
@@ -24,15 +25,15 @@ FROM   city
 SELECT ID, Name, District
 FROM   city
 
-
-SELECT ename, salary, salary * 12,
-       (salary * 12) + (salary DIV 2)
+#使用cmdev資料庫的emp表格，計算員工年薪
+SELECT ename, salary, salary * 12, #月薪乘以12計算出年薪
+       (salary * 12) + (salary DIV 2) #年薪再加上半個月的獎金
 FROM   cmdev.emp
 
-
+#ＡＳ:幫欄位/資料/運算式另取別名
 SELECT ename, salary AS MonthSalary,
        salary * 12 AS AnnualSalary,
-       (salary * 12) + (salary DIV 2) AnnualFullSalary
+       (salary * 12) + (salary DIV 2) AnnualFullSalary #不使用as,先空格再取別名
 FROM   cmdev.emp
 
 # 如果你的別名包含空格，要用單引號或雙引號
