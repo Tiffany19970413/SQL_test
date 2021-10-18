27328ee5be4ff724e3c2977d9ee7e7a7c9371282
tiffany jiang <71298683+Tiffany19970413@users.noreply.github.com>
2021-10-18 14:42:17 +0800

    Update ch04_04.sql

--- JOIN_SQLExample/ch04_04.sql
+++ JOIN_SQLExample/ch04_04.sql
@@ -1,13 +1,16 @@
+#UNION語法
+
+#獨立查詢「東北亞地區而且人口數小於 兩百萬 的國家」
 SELECT Region, Name, Population
 FROM   country
 WHERE  Region = 'Southeast Asia' AND Population < 2000000
 
-
+#獨立查詢「東北亞地區而且人口數小於 一百萬 的國家」
 SELECT Region, Name, Population
 FROM   country
 WHERE  Region = 'Eastern Asia' AND Population < 1000000
 
-
+#使用UNION語法，包含兩個查詢記錄，回傳一個查詢結果欄位
 SELECT Region, Name, Population
 FROM   country
 WHERE  Region = 'Southeast Asia' AND Population < 2000000
@@ -17,7 +20,7 @@ FROM   country
 WHERE  Region = 'Eastern Asia' AND Population < 1000000
 
 
-SELECT Region, Name, Population
+SELECT Region, Name, Population #使用第一個查詢欄位名稱
 FROM   country
 WHERE  Region = 'Southeast Asia' AND Population < 2000000
 UNION
@@ -27,28 +30,28 @@ WHERE  Region = 'Eastern Asia' AND Population < 1000000
 UNION
 SELECT ename, job, salary
 FROM   cmdev.emp
-WHERE  salary < 1000
+WHERE  salary < 1000 #慢慢查詢
 
 
-SELECT Region, Name, Population
+SELECT Region, Name, Population #查詢三個欄位
 FROM   country
 WHERE  Region = 'Southeast Asia' AND Population < 2000000
 UNION
-SELECT Region, Name, Population
+SELECT Region, Name, Population #查詢三個欄位
 FROM   country
 WHERE  Region = 'Eastern Asia' AND Population < 1000000
 UNION
-SELECT ename, job, salary, comm
+SELECT ename, job, salary, comm #所有使用UNION合併的查詢欄位個數須一樣
 FROM   cmdev.emp
 WHERE  salary < 1000
 
-
+#範例：原本查詢條件過於冗長
 SELECT Region, Name, Population
 FROM   country
 WHERE  (Region = 'Southeast Asia' AND Population < 2000000) OR
        (Region = 'Eastern Asia' AND Population < 1000000)
 
-
+#改善範例：拆開成兩個查詢條件
 SELECT Region, Name, Population
 FROM   country
 WHERE  Region = 'Southeast Asia' AND Population < 2000000
